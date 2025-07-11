1379310acebb9e1c3d2182987af593d0dfb617c0
laeti <leneveulaetitia6@gmail.com>
2025-07-11 23:39:19 +0200

    quatrieme partie et cinquieme partie

--- projet_python.py
+++ projet_python.py
@@ -4,6 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 
@@ -17,6 +21,26 @@ def lire_plant_generation_data_groupe_3(filename : str) -> pd.DataFrame | None :
         data_plant_generation = pd.read_csv(filename)
         st.success(f"Fichier trouvé : {filename}")
         return data_plant_generation 
+    except Exception as e : 
+        st.error(f"Fichier introuvable : {filename}")
+        return None
+    
+def lire_target_groupe_3(filename : str) -> pd.DataFrame | None : 
+    
+    try : 
+        target_groupe_3 = pd.read_csv(filename)
+        st.success(f"Fichier trouvé : {filename}")
+        return target_groupe_3
+    except Exception as e : 
+        st.error(f"Fichier introuvable : {filename}")
+        return None
+    
+
+def lire_plant_weather_forecast_groupe_3(filename : str) -> pd.DataFrame | None : 
+    try : 
+        plant_weather_forecast_groupe_3 = pd.read_csv(filename)
+        st.success(f"Fichier trouvé : {filename}")
+        return plant_weather_forecast_groupe_3
     except Exception as e : 
         st.error(f"Fichier introuvable : {filename}")
         return None
@@ -128,8 +152,46 @@ def tracer_heatmap_correlation(corr_variable, title="Matrice de corrélation")->
 def supprimer_module_temp(df : pd.DataFrame) -> pd.DataFrame :
      st.info("Colonne module_temperature supprimée car corrélée à irradiation")
      return df.drop("module_temperature", axis=1)
+ 
+def create_final_dataset(df : pd.DataFrame) -> pd.DataFrame : 
+    colonnes_selectionees = ["irradiation", "ambient_temperature", "Heure", "Jour"]
+    return df[colonnes_selectionees].copy()
 
+def separer_X_y(df : pd.DataFrame) -> pd.DataFrame | pd.Series : 
+    X = create_final_dataset(df)
+    y = df["dc_power"]
+    st.info("Les données ont été séparées entre variables explicatives (X) et variable cible (y)")
+    return X,y 
+
+
+def split_train_test(X : pd.DataFrame, y : pd.Series, test_size : float = 0.2, random_state : int = 42) : 
     
+    st.info("Les données ont été découpée en 80% de données d'entrainement et 20% de données de tests")
+    return train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+
+def evaluer_modele_lineaire(y_test, y_pred) -> pd.DataFrame : 
+    mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_pred)
+    
+    return pd.DataFrame({"MAE": [mae], "MSE": [mse], "RMSE": [rmse], "R²": [r2]})
+
+
+def tracer_graphique_prediction(y_test, y_pred_prev, title : str = "Production réelle VS production prédite", xlabel : str = "Puissance DC réelle", ylabel : str = "Puissance DC prédite") -> None : 
+    plt.figure(figsize=(8,5))
+    plt.scatter(y_test, y_pred_prev)
+    # bissectrice
+    min_val, max_val = y_test.min(), y_test.max()
+    plt.plot([min_val, max_val], [min_val, max_val], "--", color="red")
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    
+
+
     
 
 
@@ -155,11 +217,11 @@ with tab1:
 with tab2:
     st.subheader("Exploration des données")
     st.markdown("Exploration des données: lire les données plant_generation_data_groupe_<1, 2,3>.csv et plant_weather_data_groupe_<1, 2, 3>.csv")
-    lire_les_donnees = st.button("Lire les données")
+    lire_les_donnees = st.button("Lire les données d'explorations")
     
-    if lire_les_donnees : 
-        df_plant_generation = lire_plant_generation_data_groupe_3("plant_generation_data_groupe_3.csv")
-        df_weather_data = lire_plant_weather_data_group_3("plant_weather_data_groupe_3.csv")
+    
+    df_plant_generation = lire_plant_generation_data_groupe_3("plant_generation_data_groupe_3.csv")
+    df_weather_data = lire_plant_weather_data_group_3("plant_weather_data_groupe_3.csv")
         
     st.markdown("1. Quels sont les types de variables disponibles ?")
     
@@ -243,22 +305,76 @@ with tab4:
     st.dataframe(variable_correlee)
     st.markdown("Les deux variables corrélées entre elles sont donc irradiation et module temperature")
     st.markdown("3. Supprimer une des deux variables corrélées (la moins importante) de l'analyse")
-    df_final = supprimer_module_temp(df_correlation)
-    st.dataframe(df_final)
+    df_nettoyage = supprimer_module_temp(df_nouvelles_features)
+    st.dataframe(df_nettoyage)
     st.markdown("4. Créer une fonction create_final_dataset(df) qui prend en entrée un DataFrame et renvoie un DataFrame avec les variables sélectionnées pour l'entraînement du modèle.")
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    df_selected = create_final_dataset(df_nettoyage)
+    st.dataframe(df_selected.head())
+       
 with tab5 : 
     st.subheader("Modélisation")
+    st.markdown("1. Séparer les données en train/test (80% des données pour l'entraînement et 20% pour le test)")
+    X, y = separer_X_y(df_nettoyage)
+    X_train, X_test, y_train, y_test = split_train_test(X, y)
+    st.markdown("2. Entraîner le modèle de régression linéaire (LinearRegression) sur les données d'entraînement")
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    st.success("Les données ont bien été entrainées avec le modèle de régression linéaire !")
+    st.markdown("3. Évaluer le modèle sur les données de test")
+    y_pred = model.predict(X_test)
+    df_score = evaluer_modele_lineaire(y_test, y_pred)
+    st.success("Modèle évalué avec succès !")
+    st.markdown("4. Afficher les métriques de performance (ex: RMSE, MAE, R2)")
+    st.dataframe(df_score)
+    st.markdown("5. Entraîner un modèle de régression par forêt aléatoire (RandomForestRegressor) sur les données d'entraînement")
+    model_forest = RandomForestRegressor(random_state=42)
+    model_forest.fit(X_train, y_train)
+    st.success("Les données ont bien été entrainées avec le modèle de forêt aléatoire !")
+    st.markdown("6. Évaluer le modèle sur les données de test")
+    y_pred_random_forest = model_forest.predict(X_test)
+    df_score_forest = evaluer_modele_lineaire(y_test, y_pred_random_forest)
+    st.success("Modèle évalué avec succès !")
+    st.markdown("7. Afficher les métriques de performance (ex: RMSE, MAE, R2)")
+    st.dataframe(df_score_forest)
+    st.markdown("8. Comparer les performances des deux modèles")
+    df_comparer_modeles = pd.concat([df_score, df_score_forest])
+    st.dataframe(df_comparer_modeles)
+    st.markdown("Au vu des résultats obtenus, le modèle de forêt aléatoire semble donner de meilleurs résultats")
+      
 with tab6 : 
      st.subheader("Visualisation des résultats")
+     st.markdown("1. Lire les données de test: target_groupe_<1, 2, 3>.csv et plant_weather_forecast_groupe_<1, 2, 3>.csv")
+     st.button("Lire les données")
+     df_to_pred = lire_target_groupe_3("target_groupe_3.csv")
+     df_weather_forecast = lire_plant_weather_forecast_groupe_3("plant_weather_forecast_groupe_3.csv")
+     st.markdown("2. Fusionner les données de test avec les prévisions météo")
+     df_to_pred = fusionner_les_dataframe(df_to_pred, df_weather_forecast)
+     st.dataframe(df_to_pred)
+     st.markdown("3. Appliquer les transformations nécessaires")
+     conversion_en_date_time(df_to_pred)
+     df_to_pred_features = create_features(df_to_pred)
+     df_to_pred_selected = create_final_dataset(df_to_pred_features)
+     st.dataframe(df_to_pred_selected.head())
+     st.markdown("4. Appliquer le meilleur modèle (celui ayant la métrique R2 la plus élevée) sur les données de test")
+     y_pred_prev = model_forest.predict(X_test)
+     results = X_test.copy() 
+     results["actual"] = y_test.values     
+     results["prédit"] = y_pred_prev
+     st.dataframe(results.head())
+     tracer_graphique_prediction(y_test, y_pred_prev)
+     st.pyplot(plt)
+     
+     
+
+   
+     
+     
+     
+     
+     
+     
+     
+     
    
     
 
